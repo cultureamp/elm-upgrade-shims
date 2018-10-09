ddeefc970da3f6b1f01d59cbd324004976053a2e
Louis Quinnell <louis.quinnell@cultureamp.com>
2018-10-09 12:03:05 +1100

    Shim Browser.element

---
+++ workspaces/shims/src/Shims/Browser.elm
@@ -0,0 +1,14 @@
+module Shims.Browser exposing (element)
+
+import Html exposing (..)
+
+
+element :
+    { init : flags -> ( model, Cmd msg )
+    , view : model -> Html msg
+    , update : msg -> model -> ( model, Cmd msg )
+    , subscriptions : model -> Sub msg
+    }
+    -> Program flags model msg
+element =
+    Html.programWithFlags
