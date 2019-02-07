644e0825c1a1bd8328f3052dc4ad33678df49ae9
Dennis Hotson <dennis.hotson@cultureamp.com>
2019-02-07 11:49:49 +1100

    Shim Date.fromTime

--- workspaces/shims-0.18/src/Elm18Compatible/Date.elm
+++ workspaces/shims-0.18/src/Elm18Compatible/Date.elm
@@ -1,5 +1,8 @@
-module Elm18Compatible.Date exposing (Date)
+module Elm18Compatible.Date exposing (Date, fromTime)
 
 import Date as Date18
 
 type alias Date = Date18.Date
+type alias Time = Date18.Time
+
+fromTime = Date18.fromTime
--- workspaces/shims-0.19/src/Elm18Compatible/Date.elm
+++ workspaces/shims-0.19/src/Elm18Compatible/Date.elm
@@ -1,5 +1,9 @@
-module Elm18Compatible.Date exposing (Date)
+module Elm18Compatible.Date exposing (Date, fromTime)
 
 import Time as Time19
 
 type alias Date = Time19.Posix
+type alias Time = Float
+
+fromTime : Float -> Date
+fromTime time = Time19.millisToPosix (Basics.round time)
