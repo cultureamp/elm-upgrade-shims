27f38136eab63ba4afe60354c48a4016e38ac697
Michael Bylstra <mbylstra@gmail.com>
2019-02-25 21:16:48 +1100

    add shims-0.18 for floor, diff, compare

--- workspaces/shims-0.18/src/Elm18Compatible/Date/Extra.elm
+++ workspaces/shims-0.18/src/Elm18Compatible/Date/Extra.elm
@@ -1,11 +1,26 @@
-module Elm18Compatible.Date.Extra exposing (Interval, toIsoString)
+module Elm18Compatible.Date.Extra exposing (Interval(..), toIsoString, diff, floor, compare)
 
 import Date as Date18
 import Date.Extra as Extra18
 
 
-type alias Interval =
-    Extra18.Interval
+type Interval
+    = Year
+    | Quarter
+    | Month
+    | Week
+    | Monday
+    | Tuesday
+    | Wednesday
+    | Thursday
+    | Friday
+    | Saturday
+    | Sunday
+    | Day
+    | Hour
+    | Minute
+    | Second
+    | Millisecond
 
 
 toIsoString : Date18.Date -> String
@@ -17,13 +32,67 @@ toFormattedString =
     Extra18.toFormattedString
 
 
-floor =
-    Extra18.floor
+floor : Interval -> Date18.Date -> Date18.Date
+floor interval date =
+    Extra18.floor (convertInterval interval) date
 
 
-diff =
-    Extra18.diff
+diff : Interval -> Date18.Date -> Date18.Date -> Int
+diff interval dateA dateB =
+    Extra18.diff (convertInterval interval) dateA dateB
 
 
 compare =
     Extra18.compare
+
+
+convertInterval : Interval -> Extra18.Interval
+convertInterval interval =
+    case interval of
+        Year ->
+            Extra18.Year
+
+        Quarter ->
+            Extra18.Quarter
+
+        Month ->
+            Extra18.Month
+
+        Week ->
+            Extra18.Week
+
+        Monday ->
+            Extra18.Monday
+
+        Tuesday ->
+            Extra18.Tuesday
+
+        Wednesday ->
+            Extra18.Wednesday
+
+        Thursday ->
+            Extra18.Thursday
+
+        Friday ->
+            Extra18.Friday
+
+        Saturday ->
+            Extra18.Saturday
+
+        Sunday ->
+            Extra18.Sunday
+
+        Day ->
+            Extra18.Day
+
+        Hour ->
+            Extra18.Hour
+
+        Minute ->
+            Extra18.Minute
+
+        Second ->
+            Extra18.Second
+
+        Millisecond ->
+            Extra18.Millisecond
